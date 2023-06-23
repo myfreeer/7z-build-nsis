9bbf0d07f8bff40dc28805a3dbb0ffdc4ca3a9f2
myfreeer <myfreeer@users.noreply.github.com>
2023-06-23 11:07:31 +0800

    patch: update for nsis define
    Since 23.01
    https://github.com/myfreeer/7z-build-nsis/issues/45

--- 7-zip-patch.sh
+++ 7-zip-patch.sh
@@ -5,8 +5,8 @@ sed -i '/LFLAGS = $(LFLAGS) -OPT:NOWIN98/ c\LFLAGS = $(LFLAGS)\' CPP/Build.mak
 
 # patch NsisIn.h to enable NSIS script decompiling
 # https://sourceforge.net/p/sevenzip/discussion/45797/thread/5d10a376/
-# insert #define NSIS_SCRIPT before the 19th line using sed
-sed -i '19 i #define NSIS_SCRIPT' CPP/7zip/Archive/Nsis/NsisIn.h
+# uncomment the NSIS_SCRIPT define using sed since the line number changed in 23.01
+sed -i 's|// #define NSIS_SCRIPT|#define NSIS_SCRIPT|g' CPP/7zip/Archive/Nsis/NsisIn.h
 
 # drop -WX option in Build.mak
 # workaround error C2220: warning treated as error
